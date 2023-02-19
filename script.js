20bcd6ff6aebb201108f784aae4a71a74cefdfef
stephenChing <stephenchinag@gmail.com>
2023-02-19 01:10:08 +0100

    getting Data

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
 'use strict';
+const getCountry = (country, langAbb, currAbb) => {
+  const btn = document.querySelector('.btn-country');
+  const countriesContainer = document.querySelector('.countries');
 
-const btn = document.querySelector('.btn-country');
-const countriesContainer = document.querySelector('.countries');
+  ///////////////////////////////////////
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
 
-///////////////////////////////////////
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    const html = ` 
+  <article class="country">
+  <img class="country__img" src="${data.flags.png}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name.common}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span> ${(
+      data.population / 1000000
+    ).toFixed(1)}</p>
+    <p class="country__row"><span>🗣️</span>${data.languages[langAbb]}</p>
+    <p class="country__row"><span>💰</span>${data.currencies[currAbb].name}</p>
+  </div>
+</article>`;
+
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+getCountry('portugal', 'por', 'EUR');
+getCountry('nigeria', 'eng', 'NGN');
